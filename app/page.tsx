7db88066157f3cdbb76128b46cec897f3a9dd2f1
eldorado11sh@gmail.com <eldorado11sh@gmail.com>
2024-05-18 03:14:58 +0900

    Initial Commit - Finished

---
+++ app/page.tsx
@@ -0,0 +1,10 @@
+import ScrollIndicator from "@/components/ScrollIndicator";
+
+export default function Home() {
+  return (
+    <main className="">
+      <ScrollIndicator />
+      <div className="h-[2000px]" />
+    </main>
+  );
+}
